dd2c7f72846363a0ab041cfa6515f28eb3a49aaa
18rajritik <20ritikraj04@gmail.com>
2026-05-09 23:59:35 +0530

    Add root index page

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/views", express.static(path.join(__dirname, "views")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use("/api/auth", authRoutes);
